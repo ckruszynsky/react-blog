9617b306668bbb0bb0284b78cfe8c20369a96613
Chad Kruszynsky <ckruszynsky@gmail.com>
2017-06-13 13:28:29 -0500

    Setup Route definitions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/app/';
+import PostList from './components/posts/list.js';
+import NewPost from './components/posts/new.js';
+import PostDetails from './components/posts/details.js';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+     <BrowserRouter>
+        <div>            
+            <Route path="/posts/:id" component={PostDetails} />
+            <Route path="/posts/new" component={NewPost} />
+            <Route path="/" component={PostList} />
+            <App />
+        </div>        
+     </BrowserRouter>        
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
